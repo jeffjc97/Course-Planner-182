2c04b9ae21e4966b895a57f997c7ef8d2354a625
Tomoya Hasegawa <tomoya.hasegawa@gmail.com>
2016-12-06 14:53:35 -0500

    gened parsing

--- helpers.py
+++ helpers.py
@@ -16,6 +16,32 @@ def parse_csv():
             }
     return class_dict
 
+def parse_geneds():
+    geneds = ["AI", "CB", "EMR", "ER", "SLS", "SP", "SPU", "SW", "USW"]
+    ai_dict, cb_dict, emr_dict, er_dict, sls_dict, sp_dict, spu_dict, sw_dict, usw_dict = {}, {}, {}, {}, {}, {}, {}, {}, {}
+    dicts = [ai_dict, cb_dict, emr_dict, er_dict, sls_dict, sp_dict, spu_dict, sw_dict, usw_dict]
+
+    for i in range(len(geneds)):
+        with open(geneds[i] + '.csv', 'rb') as csvfile:
+            csvreader = csv.reader(csvfile)
+            next(csvreader, None)
+            class_dict = dicts[i]
+            prev = None
+            for line in csvreader:
+                # mark with catalog number
+                if not line[11] == "":
+                    if line[0] == "":
+                        class_dict[prev]['days'][int(line[11]) - 1] = True
+                    else:
+                        class_dict[int(line[0])] = {
+                            'class_name': geneds[i] + line[4] + line[5],
+                            'semester': [line[1] == 'FALL', line[1] == 'SPRING'],
+                            'days': [line[11]  == '1', line[11]  == '2', line[11]  == '3', line[11]  == '4', line[11]  == '5'],
+                            'times': [line[14].replace(":", "")[0:4], line[15].replace(":", "")[0:4]]
+                        }
+                        prev = int(line[0])
+    return dicts
+
 def get_slot_from_index(index):
     year = index / 12
     semester = int(index % 12 >= 6)
@@ -25,4 +51,3 @@ def get_slot_from_index(index):
         'semester': semester,
         'slot': slot
     }
-
